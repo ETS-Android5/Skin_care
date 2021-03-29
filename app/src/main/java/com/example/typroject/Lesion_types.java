dd3d52bb979c0fd02e5200a1fbdcc58080e29d81
amit <amit.kushwaha@somaiya.edu>
2021-03-29 20:21:46 +0530

    completed the skin Lesion_types module

--- app/src/main/java/com/example/typroject/Lesion_types.java
+++ app/src/main/java/com/example/typroject/Lesion_types.java
@@ -2,13 +2,18 @@ package com.example.typroject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Lesion_types extends AppCompatActivity {
-
+    ImageView imageView ;
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesion_types);
+        imageView = findViewById(R.id.Melanocytic_i);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.melanocytic));
     }
 }
