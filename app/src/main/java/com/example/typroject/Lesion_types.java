ac7dfac2411fd0940651b4b204f84c5574b7d7de
amit <amit.kushwaha@somaiya.edu>
2021-03-31 03:20:26 +0530

    updated activity_lesion_types.xml to added buttons with external link to read more information about particular lesions

--- app/src/main/java/com/example/typroject/Lesion_types.java
+++ app/src/main/java/com/example/typroject/Lesion_types.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class Lesion_types extends AppCompatActivity {
     ImageView im_home;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7;
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,77 @@ public class Lesion_types extends AppCompatActivity {
             public void onClick(View v) {
                 Intent to_home = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(to_home);
+            }
+        });
+        btn1 = findViewById(R.id.btn1_les);
+        btn2 = findViewById(R.id.btn2_les);
+        btn3 = findViewById(R.id.btn3_les);
+        btn4 = findViewById(R.id.btn4_les);
+        btn5 = findViewById(R.id.btn5_les);
+        btn6 = findViewById(R.id.btn6_les);
+        btn7 = findViewById(R.id.btn7_les);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.example.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.example.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.example.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.example.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.example.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.amazon.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.google.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
             }
         });
     }
